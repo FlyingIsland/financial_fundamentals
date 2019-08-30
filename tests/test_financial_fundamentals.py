1a1a5864cef2d0c265d58433d81611a4795bfd62
Mahesh <mahesh.s@e-arth.in>
2019-08-30 23:42:58 +0530

    working script for financial_fundamentals

---
+++ tests/test_financial_fundamentals.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from financial_fundamentals import accounting_metrics
+date_range = pd.date_range('2018-12-1', '2018-12-31')
+required_data = pd.DataFrame(columns=['MSFT'], index=date_range)
+print accounting_metrics.earnings_per_share(required_data)
